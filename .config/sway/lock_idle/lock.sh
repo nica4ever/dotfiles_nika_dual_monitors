116d4dfa971a6f6faadd96e01ff03597e77392da
Nika / Cojanu Ionut <cojanuionut9@gmail.com>
2026-03-10 20:06:58 +0200

    fix: fixed swaylock bug

--- .config/sway/lock_idle/lock.sh
+++ .config/sway/lock_idle/lock.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 pidof swaylock && exit
-grim -t ppm - | \
-magick - -scale 75% -blur 0x4 -fill black -colorize 25% -scale 133.33% -quality 90 /tmp/lockscreen.jpg && \
-swaylock -f --config ~/.config/sway/lock_idle/swaylock.conf --image /tmp/lockscreen.jpg
+
+IMAGES=""
+for output in $(swaymsg -t get_outputs -r | grep -o '"name": "[^"]*"' | cut -d'"' -f4); do
+  grim -t ppm -o "$output" - | \
+    magick - -scale 75% -blur 0x4 -fill black -colorize 25% -scale 133.33% -quality 90 "/tmp/lock_${output}.jpg"
+  IMAGES="$IMAGES -i ${output}:/tmp/lock_${output}.jpg"
+done
+
+swaylock -f --config ~/.config/sway/lock_idle/swaylock.conf $IMAGES
